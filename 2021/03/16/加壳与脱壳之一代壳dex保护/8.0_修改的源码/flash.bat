8c0878017c9af3832dff9df52caed9461c6a3029
onejane <1051103813@qq.com>
2021-06-01 10:21:12 +0800

    gitee

--- 2021/03/16/加壳与脱壳之一代壳dex保护/8.0_修改的源码/flash.bat
+++
@@ -1,6 +0,0 @@
-fastboot flash boot boot.img
-fastboot flash vendor vendor.img
-fastboot flash system_a system.img
-fastboot flash system_b system_other.img
-fastboot flash userdata userdata.img
-fastboot reboot
